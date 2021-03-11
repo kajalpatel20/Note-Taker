d503f86d6126d4c6881c6fa81c251afd7baee323
Kajal Patel <kajalpatel20@gmail.com>
2021-03-10 22:07:23 -0500

    remove extra line

--- Develop/server.js
+++ Develop/server.js
@@ -4,15 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8081;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 require('./routes/api-routes')(app);
 require('./routes/html-routes')(app);
-
-
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
